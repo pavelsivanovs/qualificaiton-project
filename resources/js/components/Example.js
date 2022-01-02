9ec7cafefa0a0653d61a4d7073b2fe1e2ffebe88
Pavels Ivanovs <paulivanov586@gmail.com>
2022-01-02 16:37:08 +0200

    proceeding with development

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 
 function Example() {
     return (
-        <div className="container">
+        <div className="container mx-auto">
             <div className="card">
-                <div className="col-md-8 justify-center text-gray-900">
+                <div className="col-md-8 text-gray-900 max-w-fit mx-auto">
                     <div className="card">
-                        <div className="card-header text-3xl underline text-red">Example Component</div>
+                        <div className="card-header text-3xl underline text-bold">Example Component</div>
 
                         <div className="card-body">I'm an example component!</div>
                     </div>
