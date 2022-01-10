f408653d2f1bcd95868024b2e3ddfebe0e2ed15c
Pavels Ivanovs <paulivanov586@gmail.com>
2022-01-10 02:44:23 +0200

    Introduced 3 new middlewares for user status checking.

---
+++ app/Http/Middleware/IsProjectManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use App\Models\UserStatus;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class IsProjectManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        if ($user->status != UserStatus::STATUS_PM) {
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
